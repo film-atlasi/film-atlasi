45703960bdad4cfe4afa71731a8d5766a91c3e3f
Muhammed Mert Bek <148920150+mertbek10@users.noreply.github.com>
2025-02-08 23:18:37 +0300

    Arama servisini ekledim ve FilmAra widget'ını güncelledim.

--- lib/features/movie/widgets/FilmAra.dart
+++ lib/features/movie/widgets/FilmAra.dart
@@ -1,9 +1,5 @@
-import 'package:film_atlasi/features/movie/services/MovieServices.dart';
-import 'package:film_atlasi/features/movie/models/Movie.dart';
-import 'package:film_atlasi/features/movie/screens/IletiPaylas.dart';
-import 'package:film_atlasi/features/user/models/User.dart';
-import 'package:film_atlasi/features/user/screens/UserPage.dart';
-import 'package:film_atlasi/features/user/services/UserServices.dart';
+import 'package:film_atlasi/features/movie/services/search_service.dart';
+import 'package:film_atlasi/features/movie/widgets/search_results.dart';
 import 'package:flutter/material.dart';
 
 class FilmAraWidget extends StatefulWidget {
@@ -14,7 +10,7 @@ class FilmAraWidget extends StatefulWidget {
 }
 
 class _FilmAraWidgetState extends State<FilmAraWidget> {
-  final MovieService _movieService = MovieService();
+  final SearchService _searchService = SearchService();
   final TextEditingController _searchController = TextEditingController();
   List<dynamic> _searchResults = [];
   bool _isLoading = false;
@@ -27,10 +23,9 @@ class _FilmAraWidgetState extends State<FilmAraWidget> {
     });
 
     try {
-      final movies = await _movieService.searchMovies(query);
-      final users = await UserServices.searchUsers(query);
+      final results = await _searchService.search(query);
       setState(() {
-        _searchResults = [...users, ...movies];
+        _searchResults = results;
       });
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -55,13 +50,12 @@ class _FilmAraWidgetState extends State<FilmAraWidget> {
           if (_isLoading)
             Center(child: const CircularProgressIndicator())
           else
-            buildSearchResults()
+            Expanded(child: SearchResults(searchResults: _searchResults))
         ],
       ),
     );
   }
 
-//filn arama butonu
   Padding buildSearchTextField() {
     return Padding(
       padding: const EdgeInsets.all(16.0),
@@ -83,71 +77,6 @@ class _FilmAraWidgetState extends State<FilmAraWidget> {
         ),
         onChanged: _searchMovies,
       ),
-    );
-  }
-
-  Expanded buildSearchResults() {
-    return Expanded(
-      child: ListView.builder(
-        itemCount: _searchResults.length,
-        itemBuilder: (context, index) {
-          final result = _searchResults[index];
-          if (result is Movie) {
-            return _buildMovieListTile(index, context);
-          } else if (result is User) {
-            return _buildUserListTile(index, context);
-          }
-        },
-      ),
-    );
-  }
-
-  ListTile _buildMovieListTile(int index, BuildContext context) {
-    final movie = _searchResults[index];
-    return ListTile(
-      leading: movie.posterPath.isNotEmpty
-          ? Image.network(
-              'https://image.tmdb.org/t/p/w92${movie.posterPath}',
-              width: 50,
-              errorBuilder: (_, __, ___) =>
-                  const Icon(Icons.movie, color: Colors.white),
-            )
-          : const Icon(Icons.movie, color: Colors.white),
-      title: Text(movie.title, style: const TextStyle(color: Colors.white)),
-      subtitle: Text(
-        movie.overview.length > 50
-            ? '${movie.overview.substring(0, 50)}...'
-            : movie.overview,
-        style: const TextStyle(color: Colors.grey),
-      ),
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => Iletipaylas(movie: movie),
-          ),
-        );
-      },
-    );
-  }
-
-  ListTile _buildUserListTile(int index, BuildContext context) {
-    final User user = _searchResults[index];
-    return ListTile(
-      leading: CircleAvatar(),
-      title: Text(user.userName ?? "username",
-          style: const TextStyle(color: Colors.white)),
-      subtitle: Text(
-        user.firstName ?? "first name",
-        style: const TextStyle(color: Colors.grey),
-      ),onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => UserPage(userUid: user.uid!,),
-          ),
-        );
-      },
     );
   }
 }
