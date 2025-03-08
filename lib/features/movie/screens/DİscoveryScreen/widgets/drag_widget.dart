18d1f24ffe00a1003ba3e51ff60f189b7177290d
Celal Dinc <celaldinc634@gmail.com>
2025-03-08 19:36:21 +0300

    Animasyon duzeltildi

--- lib/features/movie/screens/DİscoveryScreen/widgets/drag_widget.dart
+++ lib/features/movie/screens/DİscoveryScreen/widgets/drag_widget.dart
@@ -2,22 +2,19 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-enum SlideDirection {
-  left,
-  right,
-  up,
-}
+enum SlideDirection { left, right }
 
 class DragWidget extends StatefulWidget {
-  const DragWidget({
-    super.key,
-    required this.child,
-    required this.onSlideOut,
-    this.isEnableDrag = true,
-  });
+  const DragWidget(
+      {super.key,
+      required this.child,
+      this.onSlideOut,
+      this.onPressed,
+      required this.isEnableDrag});
 
   final Widget child;
-  final Function(SlideDirection direction) onSlideOut;
+  final ValueChanged<SlideDirection>? onSlideOut;
+  final VoidCallback? onPressed;
   final bool isEnableDrag;
 
   @override
@@ -26,99 +23,130 @@ class DragWidget extends StatefulWidget {
 
 class _DragWidgetState extends State<DragWidget>
     with SingleTickerProviderStateMixin {
-  late AnimationController controller;
-  late Animation<double> animation;
+  late AnimationController restoreController =
+      AnimationController(vsync: this, duration: kThemeAnimationDuration);
+  late Size screenSize = Size.zero;
 
-  Offset dragPosition = Offset.zero;
-  double dragScale = 1;
+  final _widgetKey = GlobalKey();
+  Offset startOffset = Offset.zero;
+  Offset panOffset = Offset.zero;
+
+  Size size = Size.zero;
   double angle = 0;
+
+  // To check if the card is slide
+  bool itWasMadeSlide = false;
+
+  double get outSizeLimit => size.width * 0.65;
+
+  void onPanStart(DragStartDetails details) {
+    if (!restoreController.isAnimating) {
+      setState(() {
+        startOffset = details.globalPosition;
+      });
+    }
+  }
+
+  void onPanUpdate(DragUpdateDetails details) {
+    if (!restoreController.isAnimating) {
+      setState(() {
+        panOffset = details.globalPosition - startOffset;
+        angle = currentAngle;
+      });
+    }
+  }
+
+  void onPanEnd(DragEndDetails details) {
+    if (restoreController.isAnimating) {
+      return;
+    }
+    final velocityX = details.velocity.pixelsPerSecond.dx;
+    final positionX = currentPosition.dx;
+    if (velocityX < -1000 || positionX < -outSizeLimit) {
+      itWasMadeSlide = widget.onSlideOut != null;
+      widget.onSlideOut?.call(SlideDirection.left);
+    }
+    if (velocityX > 1000 || positionX > (screenSize.width - outSizeLimit)) {
+      itWasMadeSlide = widget.onSlideOut != null;
+      widget.onSlideOut?.call(SlideDirection.right);
+    }
+    restoreController.forward();
+  }
+
+  void restoreAnimationListener() {
+    if (restoreController.isCompleted) {
+      restoreController.reset();
+      panOffset = Offset.zero;
+      itWasMadeSlide = false;
+      angle = 0;
+      setState(() {});
+    }
+  }
+
+  Offset get currentPosition {
+    final renderBox =
+        _widgetKey.currentContext?.findRenderObject() as RenderBox?;
+    return renderBox?.localToGlobal(Offset.zero) ?? Offset.zero;
+  }
+
+  double get currentAngle {
+    return currentPosition.dx < 0
+        ? (pi * 0.2) * currentPosition.dx / size.width
+        : currentPosition.dx + size.width > screenSize.width
+            ? (pi * 0.2) *
+                (currentPosition.dx + size.width - screenSize.width) /
+                size.width
+            : 0;
+  }
+
+  void getChildSize() {
+    size =
+        (_widgetKey.currentContext?.findRenderObject() as RenderBox?)?.size ??
+            Size.zero;
+  }
 
   @override
   void initState() {
-    controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 500),
-    );
-    animation = Tween<double>(begin: 0, end: 1).animate(
-      CurvedAnimation(
-        parent: controller,
-        curve: Curves.easeOutBack,
-      ),
-    );
-    controller.addListener(() {
-      setState(() {
-        dragPosition = Offset.lerp(
-          dragPosition,
-          Offset.zero,
-          animation.value,
-        )!;
-        dragScale = lerpDouble(dragScale, 1, animation.value)!;
-        angle = lerpDouble(angle, 0, animation.value)!;
-      });
+    restoreController =
+        AnimationController(vsync: this, duration: kThemeAnimationDuration)
+          ..addListener(restoreAnimationListener);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      screenSize = MediaQuery.of(context).size;
+      getChildSize();
     });
     super.initState();
   }
 
   @override
   void dispose() {
-    controller.dispose();
+    restoreController
+      ..removeListener(restoreAnimationListener)
+      ..dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    final screenWidth = size.width;
-    final screenHeight = size.height;
-
+    final child = SizedBox(key: _widgetKey, child: widget.child);
+    if (!widget.isEnableDrag) return child;
     return GestureDetector(
-      onPanStart: widget.isEnableDrag
-          ? (_) {
-              controller.stop();
-            }
-          : null,
-      onPanUpdate: widget.isEnableDrag
-          ? (details) {
-              setState(() {
-                dragPosition += details.delta;
-                final x = dragPosition.dx;
-                angle = 45 * pi / 180 * x / screenWidth;
-                dragScale = 1 - 0.1 * dragPosition.distance / screenWidth;
-              });
-            }
-          : null,
-      onPanEnd: widget.isEnableDrag
-          ? (_) {
-              final x = dragPosition.dx;
-              final y = dragPosition.dy;
-
-              if (x.abs() >= screenWidth * 0.4) {
-                widget.onSlideOut(
-                    x > 0 ? SlideDirection.right : SlideDirection.left);
-              } else if (y <= -screenHeight * 0.15) {
-                widget.onSlideOut(SlideDirection.up);
-              } else {
-                controller.forward(from: 0);
-              }
-            }
-          : null,
-      child: Transform.translate(
-        offset: dragPosition,
-        child: Transform.rotate(
-          angle: angle,
-          child: Transform.scale(
-            scale: dragScale,
-            child: widget.child,
-          ),
-        ),
+      onPanStart: onPanStart,
+      onPanUpdate: onPanUpdate,
+      onPanEnd: onPanEnd,
+      child: AnimatedBuilder(
+        animation: restoreController,
+        builder: (context, child) {
+          final value = 1 - restoreController.value;
+          return Transform.translate(
+            offset: panOffset * value,
+            child: Transform.rotate(
+              angle: angle * (itWasMadeSlide ? 1 : value),
+              child: child,
+            ),
+          );
+        },
+        child: child,
       ),
     );
   }
-}
-
-double? lerpDouble(double? a, double? b, double t) {
-  if (a == null && b == null) return null;
-  a ??= 0.0;
-  b ??= 0.0;
-  return a + (b - a) * t;
 }
